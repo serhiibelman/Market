f92063462917457ee56c62f5037c8a5f8bc8ce8b
Serhii <serhiibelman@gmail.com>
2019-03-19 17:50:37 +0200

    Create Product model

--- market/models.py
+++ market/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.FloatField()
+    picture = models.ImageField()
+    description = models.TextField()
+    in_cart = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{} - {}UAH'.format(self.title, self.price)
