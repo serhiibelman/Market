a947ce4c7491441f33e90936bc011b9633f7429c
Serhii <serhiibelman@gmail.com>
2019-04-15 13:22:38 +0300

    Edit price

--- market/models.py
+++ market/models.py
@@ -36,21 +36,22 @@ class Products(models.Model):
 class Shirt(models.Model):
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
+    price = models.FloatField()
     image = models.CharField(max_length=250)
     description = models.TextField()
     in_cart = models.BooleanField(default=False)
 
     def __str__(self):
-        return '{}'.format(self.title)
+        return '{} - ${}'.format(self.title, self.price)
 
 
 class ShirtMaterial(models.Model):
     shirt = models.ForeignKey(Shirt, on_delete=models.CASCADE)
-    price = models.FloatField()
+    price_coef = models.FloatField()
     material = models.CharField(max_length=15, choices=MATERIALS)
 
     def __str__(self):
-        return '{} - ${}'.format(self.material, self.price)
+        return '{} - ${}'.format(self.material, self.price_coef)
 
 
 class ShirtSize(models.Model):
